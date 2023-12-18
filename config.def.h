d0ea59166d2c077646aef78caad4a9451cc3842c
David Elliott <david@starjump.org>
2023-12-18 10:58:38 +0000

    tweaks

--- config.def.h
+++ config.def.h
@@ -153,7 +153,8 @@ static const Key keys[] = {
 	{ MODKEY,				XK_period,					cyclelayout,		{.i = +1 } },
 	{ MODKEY|ShiftMask,		XK_space,					togglefloating,		{0} },
 	{ MODKEY|ShiftMask,		XK_b,						togglebar,			{0} },
-	{ MODKEY|ShiftMask,		XK_f,						togglefullscr,		{0} },
+	{ MODKEY,				XK_Up,						togglefullscr,		{0} },
+	{ MODKEY,				XK_Down,					togglefullscr,		{0} },
 	{ MODKEY,				XK_0,						view,				{.ui = ~0 } },
 	{ MODKEY|ShiftMask,		XK_0,						tag,				{.ui = ~0 } },
 	{ MODKEY,				XK_Right,					viewnext,			{0} },
@@ -189,3 +190,74 @@ static const Button buttons[] = {
 	{ ClkTagBar,		MODKEY,			Button3,	toggletag,			{0} },
 };
 
+void
+setlayoutex(const Arg *arg)
+{
+	setlayout(&((Arg) { .v = &layouts[arg->i] }));
+}
+
+void
+viewex(const Arg *arg)
+{
+	view(&((Arg) { .ui = 1 << arg->ui }));
+}
+
+void
+viewall(const Arg *arg)
+{
+	view(&((Arg){.ui = ~0}));
+}
+
+void
+toggleviewex(const Arg *arg)
+{
+	toggleview(&((Arg) { .ui = 1 << arg->ui }));
+}
+
+void
+tagex(const Arg *arg)
+{
+	tag(&((Arg) { .ui = 1 << arg->ui }));
+}
+
+void
+toggletagex(const Arg *arg)
+{
+	toggletag(&((Arg) { .ui = 1 << arg->ui }));
+}
+
+void
+tagall(const Arg *arg)
+{
+	tag(&((Arg){.ui = ~0}));
+}
+
+/* signal definitions */
+/* signum must be greater than 0 */
+/* trigger signals using `xsetroot -name "fsignal:<signame> [<type> <value>]"` */
+static Signal signals[] = {
+	/* signum           function */
+	{ "focusstack",     focusstack },
+	{ "setmfact",       setmfact },
+	{ "togglebar",      togglebar },
+	{ "incnmaster",     incnmaster },
+	{ "togglefloating", togglefloating },
+	{ "focusmon",       focusmon },
+	{ "tagmon",         tagmon },
+	{ "zoom",           zoom },
+	{ "view",           view },
+	{ "viewall",        viewall },
+	{ "viewex",         viewex },
+	{ "toggleview",     view },
+	{ "toggleviewex",   toggleviewex },
+	{ "tag",            tag },
+	{ "tagall",         tagall },
+	{ "tagex",          tagex },
+	{ "toggletag",      tag },
+	{ "toggletagex",    toggletagex },
+	{ "killclient",     killclient },
+	{ "quit",           quit },
+	{ "setlayout",      setlayout },
+	{ "setlayoutex",    setlayoutex },
+	{ "floatpos",		floatpos },
+};
